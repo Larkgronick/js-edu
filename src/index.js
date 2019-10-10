4f26dd51a46f7297d4249e511cc69c9e1ef1af4c
Nikita Zhavoronkov <larkgronick@gmail.com>
2019-10-10 15:42:53 +0300

    feat: complete js-edu task

--- src/index.js
+++ src/index.js
@@ -5,10 +5,41 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
+    
+  
     focus = 'family', 
     knowsProgramming = true,
     config = {family: 4}
+   
     ) {
-      return 0;
+      if(knowsProgramming == true){
+        if (focus == 'family'){
+          return Math.ceil(800/config.family);
+        }else if(focus == 'friends'){
+          return Math.ceil(800/config.friends);
+        }else if(focus == 'normal_life'){
+          return Math.ceil(800/config.normal_life);
+        }else if(focus == 'profession'){
+          return Math.ceil(800/config.profession);
+        }else if(focus == 'carrier'){
+          return Math.ceil(800/config.carrier);
+        }else if(focus == 'top_peformance'){
+        return Math.ceil(800/config.top_peformance);
+        }
+      } else {
+        if (focus == 'family'){
+          return Math.ceil(1300/config.family);
+        }else if(focus == 'friends'){
+          return Math.ceil(1300/config.friends);
+        }else if(focus == 'normal_life'){
+          return Math.ceil(1300/config.normal_life);
+        }else if(focus == 'profession'){
+          return Math.ceil(1300/config.profession);
+        }else if(focus == 'carrier'){
+          return Math.ceil(1300/config.carrier);
+        }else if(focus == 'top_peformance'){
+        return Math.ceil(1300/config.top_peformance);
+        }
+      }
   };
   
